f008c6d1eb97dfe835dda2ff3ef25a7e7079c344
Simon Morvan <garphy@zone84.net>
2015-09-16 20:24:40 +0200

    Dockerized settings.php & drushrc.php

---
+++ www/sites/default/drushrc.php
@@ -0,0 +1,14 @@
+<?php
+
+$options['uri'] = getenv('DRUPAL_EXTERNAL_URI');
+$options['sites-subdir'] = 'default';
+
+
+$options['variables']['language_default'] = (object) array(
+  'language' => 'en',
+  'name' => 'English',
+  'native' => 'English',
+  'direction' => '0',
+  'enabled' => '1',
+  'prefix' => 'en',
+);
